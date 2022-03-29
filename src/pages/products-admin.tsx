c6a3b2b121ae27b8b9a40b917c180245e7441a42
orkhai <dicksonorkhai@gmail.com>
2022-03-29 13:54:28 +0100

    fixed copying to clipboard

--- src/pages/products-admin.tsx
+++ src/pages/products-admin.tsx
@@ -109,7 +109,7 @@ const ProductAdmin = () => {
                         <h4 className="title">Filter</h4>
                       </div>
                       <div className="filter-btn">
-                        <a className="btn btn-primary" href="/#">
+                        <a className="btn btn-primary" href="#/">
                           Reset
                         </a>
                       </div>
@@ -218,12 +218,12 @@ const ProductAdmin = () => {
                         <nav className="justify-content-end d-flex">
                           <ul className="pagination">
                             <li className="page-item disabled">
-                              <a className="page-link" href="/#" tabIndex={-1}>
+                              <a className="page-link" href="#/" tabIndex={-1}>
                                 Previous
                               </a>
                             </li>
                             <li className="page-item">
-                              <a className="page-link" href="/#">
+                              <a className="page-link" href="#/">
                                 1
                               </a>
                             </li>
@@ -231,17 +231,17 @@ const ProductAdmin = () => {
                               className="page-item active"
                               aria-current="page"
                             >
-                              <a className="page-link" href="/#">
+                              <a className="page-link" href="#/">
                                 2
                               </a>
                             </li>
                             <li className="page-item">
-                              <a className="page-link" href="/#">
+                              <a className="page-link" href="#/">
                                 3
                               </a>
                             </li>
                             <li className="page-item">
-                              <a className="page-link" href="/#">
+                              <a className="page-link" href="#/">
                                 Next
                               </a>
                             </li>
